e19e9db9f304bf211044f36c1abd5e2d160927b3
amanhanda1 <90999980+amanhanda1@users.noreply.github.com>
2025-05-08 13:10:06 +0530

    runnable advanced

---
+++ Runnable_lambda.py
@@ -0,0 +1,42 @@
+#it is used to convert python function into a runnable
+from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableLambda, RunnablePassthrough, RunnableParallel
+
+load_dotenv()
+
+def word_count(text):
+    return len(text.split())
+
+prompt = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+llm=HuggingFaceEndpoint(
+    repo_id="microsoft/Phi-3-mini-4k-instruct",
+    task="text-generation",
+    max_new_tokens=512,
+    temperature=1.8
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = StrOutputParser()
+
+joke_gen_chain = RunnableSequence(prompt, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'word_count': RunnableLambda(word_count)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+result = final_chain.invoke({'topic':'AI'})
+
+final_result = """{} \n word count - {}""".format(result['joke'], result['word_count'])
+
+print(final_result)
---
+++ runnable_conditional.py
@@ -0,0 +1,39 @@
+from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableLambda, RunnablePassthrough, RunnableBranch
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Summarize the following text \n {text}',
+    input_variables=['text']
+)
+
+llm=HuggingFaceEndpoint(
+    repo_id="microsoft/Phi-3-mini-4k-instruct",
+    task="text-generation",
+    max_new_tokens=512,
+    temperature=1.8
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = StrOutputParser()
+
+report_gen_chain = prompt1 | model | parser
+
+branch_chain = RunnableBranch(
+    (lambda x: len(x.split())>300, prompt2 | model | parser),#if condition
+    RunnablePassthrough() #else condition
+)
+
+final_chain = RunnableSequence(report_gen_chain, branch_chain) #joining the runnables
+
+print(final_chain.invoke({'topic':'Russia vs Ukraine'}))
